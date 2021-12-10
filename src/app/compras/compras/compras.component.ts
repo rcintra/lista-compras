758fb3470c90fd32d433d1d4a82e0d9108b3dbd7
Rafael Cintra <ra.cintra@gmail.com>
2021-12-10 17:09:47 -0300

    new item

--- src/app/compras/compras/compras.component.ts
+++ src/app/compras/compras/compras.component.ts
@@ -11,7 +11,7 @@ export class ComprasComponent implements OnInit {
   public compras: Compra[] = [
     { id: "1", title: "item 1", complete: false },
     { id: "2", title: "item 2", complete: false },
-    { id: "3", title: "item 3", complete: false }
+    { id: "3", title: "item 23", complete: false }
   ];
   public displayedColumns: string[] = ['title'];
 
